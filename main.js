b5f1e1dc7fe3f9d645ce9566919b6a73bd69cae0
LulucaMaluca <163294645+LulucaMaluca@users.noreply.github.com>
2024-03-27 08:20:07 -0300

    ;

--- main.js
+++ main.js
@@ -9,4 +9,9 @@ while (nomeUsuario == ''){
  nomeUsuario = prompt('Qual seu nome?');
 }
 
+if (nomeUsuario == null) {
+    texto.textContent = 'usuario' ;
+} else{
+
 texto.textContent = nomeUsuario;
+}
