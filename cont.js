eca82c9e826c5a9a94840982852f57e7e1360d13
LulucaMaluca <163294645+LulucaMaluca@users.noreply.github.com>
2024-04-15 11:41:54 -0300

    aa

--- cont.js
+++ cont.js
@@ -1,16 +1,18 @@
 const botoes = document.querySelectorAll('.objetivos');
+const textos = document.querySelectorAll('.aba-conteudo')
 //botoes.length
 
 for(let i = 0; i <botoes.length; i++ ){
-console.log(i);
 botoes[i].onclick = function(){
     for(let j= 0; j < botoes.length; j++){
         botoes[j].classList.remove('ativo');
-        texto{j}.classList.remove('ativo');
+        textos{j}.classList.remove('ativo');
     }
     botoes[i].classList.add('ativo');
+    textos[j].classList.add('ativa');
 
 }
 
 //botoes[i].classList.remove('ativo');
 }
+const contadores = document.querySelectorAll('.contador')
