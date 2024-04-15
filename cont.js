c4a1005e9a491fcf33cc1811733a83461537da41
LulucaMaluca <163294645+LulucaMaluca@users.noreply.github.com>
2024-04-15 11:56:55 -0300

    a

--- cont.js
+++ cont.js
@@ -6,10 +6,10 @@ for(let i = 0; i <botoes.length; i++ ){
 botoes[i].onclick = function(){
     for(let j= 0; j < botoes.length; j++){
         botoes[j].classList.remove('ativo');
-        textos{j}.classList.remove('ativo');
+        textos[j].classList.remove('ativo');
     }
     botoes[i].classList.add('ativo');
-    textos[j].classList.add('ativa');
+    textos[i].classList.add('ativa');
 
 }
 
