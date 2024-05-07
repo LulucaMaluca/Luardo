85c20411728717ef2fcd68e2f11d7c9f82322ea7
LulucaMaluca <163294645+LulucaMaluca@users.noreply.github.com>
2024-05-07 08:51:55 -0300

    aa

--- cont.js
+++ cont.js
@@ -16,10 +16,10 @@ botoes[i].onclick = function(){
 //botoes[i].classList.remove('ativo');
 }
 const contadores = document.querySelectorAll('.contador');
-const tempoObjetivo1 = new Date ('2024-04-16T00:00:00');
-const tempoObjetivo2 = new Date ('2024-04-16T00:00:00');
-const tempoObjetivo3 = new Date ('2024-04-16T00:00:00');
-const tempoObjetivo4 = new Date ('2024-04-16T00:00:00');
+const tempoObjetivo1 = new Date ('2034-04-22T00:00:00');
+const tempoObjetivo2 = new Date ('2028-12-17T00:00:00');
+const tempoObjetivo3 = new Date ('2030-04-16T00:00:00');
+const tempoObjetivo4 = new Date ('2024-12-13T00:00:00');
 
 let tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4  ];
 for(let i =0; i< contadores.length; i++){
